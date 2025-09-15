74c3863de68ee64a50fdf78104e98de8aca796ef
김승민 <tmdals6102@gmail.com>
2025-09-15 10:21:27 +0900

    JPA @Query update, delete

--- src/main/java/kr/ac/kopo/kkssmm/springboot_jdbctest1/exam03Controller/Exam03Controller.java
+++ src/main/java/kr/ac/kopo/kkssmm/springboot_jdbctest1/exam03Controller/Exam03Controller.java
@@ -5,10 +5,7 @@ import kr.ac.kopo.kkssmm.springboot_jdbctest1.repository.MemberRepository03;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/exam03")
@@ -32,6 +29,25 @@ public class Exam03Controller {
     @PostMapping("/insert")
     public String insertMethod(@ModelAttribute("member") Member member){
         repository03.insertMethod(member.getName(), member.getAge(), member.getEmail());
+        return "redirect:/exam03";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editMethod(Model model, @PathVariable(name = "id") int id){
+        Member member = repository03.selectMethodById(id);
+        model.addAttribute("member",member);
+        return "viewPage03_edit";
+    }
+
+    @PostMapping("/update")
+    public String updateMethod(@ModelAttribute("member") Member member){
+        repository03.updateMethod(member.getName(), member.getAge(), member.getEmail(), member.getId());
+        return "redirect:/exam03";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteMethod(@PathVariable(name = "id") int id){
+        repository03.deleteMethod(id);
         return "redirect:/exam03";
     }
 }
